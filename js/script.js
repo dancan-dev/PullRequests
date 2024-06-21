a8408f0129d3a45e3906140836fafc947fc4b3e8
Dancan Tsuma <56245008+IanBriton@users.noreply.github.com>
2024-06-21 13:39:57 +0300

    Update script.js
    Check out the JS and file make some adjustments as stated.  It is too basic, some more code needs to be added.

--- js/script.js
+++ js/script.js
@@ -5,3 +5,5 @@ toggle.addEventListener('click', () => {
   toggle.classList.toggle('active')
   navigation.classList.toggle('active')
 })
+//Add more functionalities to this JS file to make the page more interactive. 
+//You can add things like courasel, more interactivity in the page among other best practises as per our company. 
